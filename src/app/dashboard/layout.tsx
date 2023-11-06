310569469f152f673625602672b4c1d485783fa2
Josiel Perez <josielp0218@gmail.com>
2023-11-06 02:20:05 -0500

    added security where the user cant access dashboard without signing in

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,45 @@
-import DashHeader from '@/components/dashheader/DashHeader';
-import styles from './styles.module.css'
-import NavBar from '@/components/navbar/NavBar';
-import React from 'react'
+"use client";
+import DashHeader from "@/components/dashheader/DashHeader";
+import styles from "./styles.module.css";
+import NavBar from "@/components/navbar/NavBar";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-function DashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const [isLoading, setLoading] = useState(true); // State to handle loading
+  const router = useRouter();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoading(false);
+      } else {
+        setTimeout(() => {
+          alert("You must be logged in to view the dashboard.");
+          router.push("/login");
+        }, 100); // 1 second delay
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth, router]);
+
+  if (isLoading) {
     return (
-        <>
-        <DashHeader/>
-        <NavBar navId='dashboard'/>
-        <main className={styles.main}>
-            {children}
-        </main>
-        </>
-    )
+      <div className={styles.loading}>
+        <img className={styles.logo_spinner} src="/logo.png" alt="logo" />
+      </div>
+    );
+  }
+  return (
+    <>
+      <DashHeader />
+      <NavBar navId="dashboard" />
+      <main className={styles.main}>{children}</main>
+    </>
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
